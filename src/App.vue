0efb7342226954bd81be4758769b58507df1f900
Nick Tiberi <nicktiberi@gmail.com>
2024-01-27 16:57:51 -0500

    build of initial invoice generator functionality

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,254 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, ref } from 'vue';
+
+const currencyFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
+
+enum ViewMode {
+  Invoice,
+  Form
+}
+
+type InvoiceCache = {
+  invoiceNumber: number;
+  billFrom: string;
+  billTo: string;
+  item: string;
+  description: string;
+  quantity: number;
+  unitCost: number;
+  notes: string;
+};
+
+let invoiceCache: InvoiceCache | undefined;
+
+const cache = localStorage.getItem('invoiceData');
+if (cache) {
+  invoiceCache = JSON.parse(cache);
+}
+
+const viewMode = ref<ViewMode>(ViewMode.Invoice);
+
+const invoiceNumber = ref(invoiceCache ? invoiceCache.invoiceNumber : 1);
+const date = ref(new Date());
+const billFrom = ref(invoiceCache ? invoiceCache.billFrom : '');
+const billTo = ref(invoiceCache ? invoiceCache.billTo : '');
+const item = ref(invoiceCache ? invoiceCache.item : '');
+const description = ref(invoiceCache ? invoiceCache.description : '');
+const quantity = ref(invoiceCache ? invoiceCache.quantity : 0);
+const unitCost = ref(invoiceCache ? invoiceCache.unitCost : 0);
+const notes = ref(invoiceCache ? invoiceCache.notes : '');
+
+const formattedDate = computed(() => date.value.toLocaleDateString('en-us'));
+const formattedInvoiceNumber = computed(() => `${date.value.toISOString().substring(0, 10)}-${invoiceNumber.value}`);
+const formattedBillFrom = computed(() => billFrom.value.replace(/\n/g, '<br />'));
+const formattedBillTo = computed(() => billTo.value.replace(/\n/g, '<br />'));
+const formattedUnitCost = computed(() => currencyFormat.format(unitCost.value));
+const formattedTotal = computed(() => currencyFormat.format(quantity.value * unitCost.value));
+const formattedNotes = computed(() => notes.value.replace(/\n/g, '<br />'));
+const displayButtonText = computed(() => viewMode.value === ViewMode.Invoice ? 'Edit Invoice' : 'Generate Invoice');
+
+const toggleDisplay = () => {
+  const newViewMode = viewMode.value === ViewMode.Form ? ViewMode.Invoice : ViewMode.Form;
+
+  if (newViewMode === ViewMode.Invoice) {
+    const invoiceData = {
+      invoiceNumber: invoiceNumber.value,
+      billFrom: billFrom.value,
+      billTo: billTo.value,
+      item: item.value,
+      description: description.value,
+      quantity: quantity.value,
+      unitCost: unitCost.value,
+      notes: notes.value
+    }
+
+    localStorage.setItem('invoiceData', JSON.stringify(invoiceData));
+  }
+
+  viewMode.value = newViewMode;
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container my-5">
+    <div v-if="viewMode === ViewMode.Form" class="row">
+      <div class="col">
+        <h1>Invoice Generator</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="mb-3">
+          <label for="invoiceNumber" class="form-label">Invoice Number</label>
+          <input v-model="invoiceNumber" type="number" class="form-control" id="invoiceNumber">
+        </div>
+
+        <div class="mb-3">
+          <label for="billFrom" class="form-label">Bill From</label>
+          <textarea v-model="billFrom" class="form-control" id="billFrom" rows="4"></textarea>
+        </div>
+
+        <div class="mb-3">
+          <label for="billTo" class="form-label">Bill To</label>
+          <textarea v-model="billTo" class="form-control" id="billTo" rows="4"></textarea>
+        </div>
+
+        <div class="mb-3">
+          <label for="item" class="form-label">Item</label>
+          <input v-model="item" type="text" class="form-control" id="item">
+        </div>
+
+        <div class="mb-3">
+          <label for="description" class="form-label">Description</label>
+          <input v-model="description" type="text" class="form-control" id="description">
+        </div>
+
+        <div class="mb-3">
+          <label for="quantity" class="form-label">Quantity</label>
+          <input v-model="quantity" type="number" class="form-control" id="quantity">
+        </div>
+
+        <div class="mb-3">
+          <label for="unitCost" class="form-label">Unit Cost</label>
+          <input v-model="unitCost" type="number" class="form-control" id="unitCost">
+        </div>
+
+        <div class="mb-3">
+          <label for="notes" class="form-label">Notes/Memo</label>
+          <textarea v-model="notes" class="form-control" id="notes" rows="4"></textarea>
+        </div>
+      </div>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="row mb-5 d-print-none">
+      <div class="col">
+        <button @click="toggleDisplay" type="button" class="btn btn-primary btn-lg">{{ displayButtonText }}</button>
+      </div>
+    </div>
+
+    <div v-if="viewMode === ViewMode.Invoice">
+      <div class="row">
+        <div class="col">
+          <h5 class="fw-bold">Bill From:</h5>
+          <address v-html="formattedBillFrom">
+          </address>
+        </div>
+
+        <div class="col text-end">
+          <img class="logo" src="./assets/eepf-logo.png" width="150" height="150" />
+        </div>
+      </div>
+
+      <hr />
+
+      <div class="row my-5">
+        <div class="col">
+          <h5 class="fw-bold">Bill To:</h5>
+          <address v-html="formattedBillTo">
+          </address>
+        </div>
+
+        <div class="col">
+          <table class="table table-borderless">
+            <tbody>
+              <tr>
+                <th scope="row">
+                  Invoice #
+                </th>
+                <td>
+                  {{ formattedInvoiceNumber }}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">
+                  Invoice Date
+                </th>
+                <td>
+                  {{ formattedDate }}
+                </td>
+              </tr>
+              <tr class="table-secondary">
+                <th scope="row">
+                  Amount Due
+                </th>
+                <th scope="row">
+                  {{ formattedTotal }}
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div class="row my-5">
+        <div class="col">
+          <table class="table">
+            <thead>
+              <tr class="table-secondary">
+                <th>
+                  Item
+                </th>
+                <th>
+                  Description
+                </th>
+                <th>
+                  Quantity
+                </th>
+                <th>
+                  Unit Cost
+                </th>
+                <th>
+                  Line Total
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  {{ item }}
+                </td>
+                <td>
+                  {{ description }}
+                </td>
+                <td>
+                  {{ quantity }}
+                </td>
+                <td>
+                  {{ formattedUnitCost }}
+                </td>
+                <td>
+                  {{ formattedTotal }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div class="row my-5">
+        <div class="col">
+          <h5 class="fw-bold">Notes/Memo</h5>
+          <p v-html="formattedNotes"></p>
+        </div>
+
+        <div class="col">
+          <table class="table table-borderless">
+            <tbody>
+              <tr class="table-secondary">
+                <th scope="row">
+                  Total
+                </th>
+                <th scope="row">
+                  {{ formattedTotal }}
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
